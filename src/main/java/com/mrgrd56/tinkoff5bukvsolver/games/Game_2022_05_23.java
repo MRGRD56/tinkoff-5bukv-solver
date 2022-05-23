0c180dcf050dfc8ea1296467720d4351ccfead1e
k.golikov <kgolikov@37bytes.com>
2022-05-23 19:26:31 +0500

    Game 2022-05-23

---
+++ src/main/java/com/mrgrd56/tinkoff5bukvsolver/games/Game_2022_05_23.java
@@ -0,0 +1,27 @@
+package com.mrgrd56.tinkoff5bukvsolver.games;
+
+import com.mrgrd56.tinkoff5bukvsolver.games.abstractions.Game;
+import com.mrgrd56.tinkoff5bukvsolver.games.builders.MispositionedLettersBuilder;
+
+import java.util.Map;
+import java.util.Set;
+
+public class Game_2022_05_23 extends Game {
+    @Override
+    public Character[] getFoundLetters() {
+        return parseFoundLetters("----а");
+    }
+
+    @Override
+    public Set<Character> getAbsentLetters() {
+        return parseAbsentLetters("щепквхтлин");
+    }
+
+    @Override
+    public Map<Character, Set<Integer>> getMispositionedLetters() {
+        return new MispositionedLettersBuilder()
+                .letter('а', 1)
+                .letter('д', 0)
+                .build();
+    }
+}
